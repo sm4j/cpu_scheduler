aa3ad7006ea25dadbc36f74e2aa025dce9567872
Jordan <jsmall2018@fau.edu>
2024-10-15 18:01:56 -0400

    Introduced a more consistent method of calculating wait time for FCFS/SJF. Implemented MLFQ and necessary process attributes/helper functions.

--- cpu_scheduler/Source.cpp
+++ cpu_scheduler/Source.cpp
@@ -14,10 +14,12 @@ private:
     vector<int> bursts;
     int burst_index;
 
+    int totalRunTime;
     int waitTime;
     int turnaroundTime;
     int responseTime;
     int timeEnteringReady; // for sjf
+    int level_counter; //for mlfq
 
     int state;
 
@@ -35,6 +37,8 @@ public:
     int get_responseTime();
     int get_state();
     int get_timeEnteringReady();
+    int get_level_counter();
+    int get_total_run();
 
     // Setters
     void set_waitTime(int waitTime);
@@ -43,17 +47,27 @@ public:
     void set_state(int state);
     void set_burst_index(int burst_index);
     void set_timeEnterReady(int timeEnterReady);
+
+    // Helpers
+    void increment_level_counter();
+    void reset_level_counter();
+    int calculate_total_run();
+
 };
 
 // Scheduling Algorithms
 vector<Process> FCFS(vector<Process> processes);
 vector<Process> SJF(vector<Process> processes);
+vector<Process> MLFQ(vector<Process> processes);
 
 // Helper Functions
 void sort_process_by_id(vector<Process>& processes);
 void print_results(vector<Process> processes);
 void print_averages(vector<Process> processes);
 void sort_process_by_next_cpu_burst(list<Process>& ready_list);
+void sort_process_by_queue_level(list<Process>& ready_list);
+void calculate_wait_time(vector<Process>& processes);
+int get_preemption_time(int level_counter);
 
 int main() {
 
@@ -76,20 +90,11 @@ int main() {
     //print_results(complete_sjf);
     //print_averages(complete_sjf);
 
+    //vector <Process> complete_mlfq = MLFQ(processes);
+    //print_results(complete_mlfq);
+    //print_averages(complete_mlfq);
+
     return 0;
-
-}
-
-Process::Process(int id, vector<int> bursts) {
-
-    this->id = id;
-    this->bursts = bursts;
-    this->burst_index = 0;
-    this->waitTime = 0;
-    this->turnaroundTime = 0;
-    this->responseTime = -1;
-    this->state = 1; // 1 = ready, 2 = running, 3 = i/o, 4 = completed
-    this->timeEnteringReady = 0;
 
 }
 
@@ -112,13 +117,6 @@ vector<Process> FCFS(vector<Process> processes) {
     while (completed_list.size() < processes.size()) {
 
         cout << "Time: " << time << endl;
-
-        // Increment wait time for processes in the ready list
-        if (ready_list.size() > 1) {
-            for (auto it = next(ready_list.begin()); it != ready_list.end(); ++it) {
-                it->set_waitTime(it->get_waitTime() + 1);
-            }
-        }
 
         // Check for a running process
         if (!current_process && !ready_list.empty()) {
@@ -189,9 +187,12 @@ vector<Process> FCFS(vector<Process> processes) {
         // Print Ready List
         cout << "Ready List: [";
         for (auto it = ready_list.begin(); it != ready_list.end(); it++) {
-            cout << "{Process " << it->get_id() << ", Upcoming CPU Burst: " << it->get_bursts()[it->get_burst_index()] << "}";
-            if (next(it) != ready_list.end()) {
-                cout << ", ";
+            if (it != ready_list.begin()) {
+                cout << "{Process " << it->get_id() << ", ";
+                cout << "Upcoming CPU Burst: " << it->get_bursts()[it->get_burst_index()] << "}";
+                if (next(it) != ready_list.end()) {
+                    cout << ", ";
+                }
             }
         }
         cout << "]" << endl;
@@ -199,7 +200,8 @@ vector<Process> FCFS(vector<Process> processes) {
         // Print I/O List
         cout << "I/O List: [";
         for (auto it = io_list.begin(); it != io_list.end(); it++) {
-            cout << "{Process " << it->get_id() << ", I/O Burst remaining: " << it->get_bursts()[it->get_burst_index()] << "}";
+            cout << "{Process " << it->get_id() << ", ";
+            cout << "I/O Burst remaining: " << it->get_bursts()[it->get_burst_index()] << "}";
             if (next(it) != io_list.end()) {
                 cout << ", ";
             }
@@ -210,13 +212,13 @@ vector<Process> FCFS(vector<Process> processes) {
 
     }
 
+    calculate_wait_time(completed_list);
     cout << endl << "Total run time: " << time << " units" << endl;
     double cpuUtilPercent = (double)cpuUtil / time * 100;
     cout << "CPU Utilization: " << cpuUtilPercent << "%" << endl << endl;
     return completed_list;
 
 }
-
 
 vector<Process> SJF(vector<Process> processes) {
 
@@ -237,15 +239,6 @@ vector<Process> SJF(vector<Process> processes) {
     while (completed_list.size() < processes.size()) {
 
         cout << "Time: " << time << endl;
-
-        // Increment wait time for processes in the ready list
-        if (ready_list.size() > 1) {
-            for (auto it = next(ready_list.begin()); it != ready_list.end(); ++it) {
-                if (it->get_state() == 1) {
-                    it->set_waitTime(it->get_waitTime() + 1);
-                }
-            }
-        }
 
         // Check for a running process
         if (!current_process && !ready_list.empty()) {
@@ -341,6 +334,7 @@ vector<Process> SJF(vector<Process> processes) {
         time++; // Increment time
     }
 
+    calculate_wait_time(completed_list);
     cout << endl << "Total run time: " << time << " units" << endl;
     double cpuUtilPercent = (double)cpuUtil / time * 100;
     cout << "CPU Utilization: " << cpuUtilPercent << "%" << endl << endl;
@@ -348,6 +342,151 @@ vector<Process> SJF(vector<Process> processes) {
 
 }
 
+vector<Process> MLFQ(vector<Process> processes) {
+
+    list<Process> ready_list; // Combined ready queue
+    list<Process> io_list;    // I/O Queue
+    vector<Process> completed_list;
+
+    int time = 0;
+    int cpuUtil = 0;
+    Process* current_process = nullptr; // Pointer for the currently running process
+    int remaining_tq = 0; // Variable to track remaining time quantum
+
+    // Initialize the ready list
+    for (auto& process : processes) {
+        ready_list.push_back(process);
+    }
+
+    while (completed_list.size() < processes.size()) {
+
+        cout << "Time: " << time << endl;
+
+        // Check for a running process
+        if (!current_process && !ready_list.empty()) {
+            sort_process_by_queue_level(ready_list);
+            current_process = &ready_list.front(); // Get the first process in the ready list
+            current_process->set_state(2); // Set to running
+
+            if (current_process->get_responseTime() == -1) {
+                current_process->set_responseTime(time);
+            }
+
+            remaining_tq = get_preemption_time(current_process->get_level_counter());
+
+        }
+
+        // When running the current process
+        if (current_process) {
+
+            current_process->get_bursts()[current_process->get_burst_index()] -= 1;
+            cpuUtil++;
+            remaining_tq--;
+
+            cout << "Ran: [Process " << current_process->get_id() << ", CPU Burst remaining: " << current_process->get_bursts()[current_process->get_burst_index()] << "]" << endl;
+
+            if (remaining_tq < 11) {
+                cout << "Remaining tq: " << remaining_tq << endl;
+            }
+            else {
+                cout << "Remaining tq: Burst is in Queue 3 and will run until finished" << endl;
+            }
+
+            // If the process did not finish and the quantum has expired
+            if (remaining_tq == 0 && current_process->get_bursts()[current_process->get_burst_index()] != 0) {
+
+                current_process->increment_level_counter();
+                ready_list.push_back(*current_process);
+                ready_list.pop_front();
+                current_process = nullptr;
+
+            }
+
+            // Check if the CPU burst is finished
+            if ((current_process) && current_process->get_bursts()[current_process->get_burst_index()] == 0) {
+                current_process->set_burst_index(current_process->get_burst_index() + 1);
+
+                // Check if the burst index is still valid before accessing
+                if (current_process->get_burst_index() < current_process->get_bursts().size()) {
+                    // If it is an I/O burst
+                    if (current_process->get_burst_index() % 2 != 0) {
+                        current_process->set_state(3); // Set to I/O
+                        io_list.push_back(*current_process); // Move to I/O queue
+                    }
+                    else {
+                        // Completed process
+                        current_process->set_state(4);
+                        current_process->set_turnaroundTime(time + 1);
+                        completed_list.push_back(*current_process);
+                        cout << "***Process " << current_process->get_id() << " completed at time " << time + 1 << "***" << endl;
+                    }
+                }
+                else {
+                    // Process has completed all bursts
+                    current_process->set_state(4);
+                    current_process->set_turnaroundTime(time + 1);
+                    completed_list.push_back(*current_process);
+                    cout << "***Process " << current_process->get_id() << " completed at time " << time + 1 << "***" << endl;
+                }
+
+                current_process->reset_level_counter();
+                ready_list.pop_front();
+                current_process = nullptr;
+            }
+        }
+
+        // Handle I/O processes
+        if (!io_list.empty()) {
+            for (auto it = io_list.begin(); it != io_list.end();) {
+                it->get_bursts()[it->get_burst_index()] -= 1;
+
+                // Check if I/O burst is finished
+                if (it->get_bursts()[it->get_burst_index()] == 0) {
+                    it->set_state(1); // Set to ready
+                    it->set_burst_index(it->get_burst_index() + 1);
+                    it->set_timeEnterReady(time);
+                    // Ensure the new burst index is valid
+                    if (it->get_burst_index() < it->get_bursts().size()) {
+                        ready_list.push_back(*it); // Move back to ready_list
+                    }
+                    it = io_list.erase(it); // Remove from I/O list
+                }
+                else {
+                    ++it; // Move to the next element
+                }
+            }
+        }
+
+        // Print Ready List
+        cout << "Ready List: [";
+        for (auto it = ready_list.begin(); it != ready_list.end(); ++it) {
+            cout << "{Process " << it->get_id() << ", Upcoming CPU Burst: " << it->get_bursts()[it->get_burst_index()] << "}";
+            if (next(it) != ready_list.end()) {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+
+        // Print I/O List
+        cout << "I/O List: [";
+        for (auto it = io_list.begin(); it != io_list.end(); ++it) {
+            cout << "{Process " << it->get_id() << ", I/O Burst remaining: " << it->get_bursts()[it->get_burst_index()] << "}";
+            if (next(it) != io_list.end()) {
+                cout << ", ";
+            }
+        }
+        cout << "]" << endl;
+
+        time++; // Increment time
+    }
+
+    calculate_wait_time(completed_list);
+    cout << endl << "Total run time: " << time << " units" << endl;
+    double cpuUtilPercent = (double)cpuUtil / time * 100;
+    cout << "CPU Utilization: " << cpuUtilPercent << "%" << endl << endl;
+    return completed_list;
+
+}
 
 void print_results(vector<Process> processes) {
 
@@ -363,6 +502,13 @@ void print_results(vector<Process> processes) {
 
 }
 
+void calculate_wait_time(vector<Process>& processes) {
+
+    for (int i = 0; i < processes.size(); i++) {
+        processes[i].set_waitTime(processes[i].get_turnaroundTime() - processes[i].get_total_run());
+    }
+
+}
 void sort_process_by_id(vector<Process>& processes) {
     sort(processes.begin(), processes.end(), [](Process a, Process b) {
         return a.get_id() < b.get_id();
@@ -391,7 +537,6 @@ void print_averages(vector<Process> processes) {
 
 }
 
-
 void sort_process_by_next_cpu_burst(list<Process>& ready_list) {
     ready_list.sort([](Process& a, Process& b) {
         // First compare the upcoming CPU burst
@@ -403,6 +548,42 @@ void sort_process_by_next_cpu_burst(list<Process>& ready_list) {
         });
 }
 
+void sort_process_by_queue_level(list<Process>& ready_list) {
+    ready_list.sort([](Process& a, Process& b) {
+        // First compare the current level
+        if (a.get_level_counter() != a.get_level_counter()) {
+            return a.get_bursts()[a.get_burst_index()] < b.get_bursts()[b.get_burst_index()];
+        }
+        // If they are equal, compare timeEnteringReady (for MLFQ)
+        return a.get_timeEnteringReady() < b.get_timeEnteringReady();
+        });
+
+}
+
+int get_preemption_time(int level_counter) {
+    switch (level_counter) {
+    case 0: return 5;  // RR with tq = 5
+    case 1: return 10; // RR with tq = 10
+    case 2: return INT_MAX; // FCFS (runs to completion)
+    default: return 5;  // Default case (should not happen)
+    }
+}
+
+// Constructor
+Process::Process(int id, vector<int> bursts) {
+
+    this->id = id;
+    this->bursts = bursts;
+    this->burst_index = 0;
+    this->waitTime = 0;
+    this->turnaroundTime = 0;
+    this->responseTime = -1;
+    this->state = 1; // 1 = ready, 2 = running, 3 = i/o, 4 = completed
+    this->timeEnteringReady = 0;
+    this->level_counter = 0;
+    this->totalRunTime = calculate_total_run();
+
+}
 
 // Getters
 int Process::get_id() {
@@ -437,6 +618,14 @@ int Process::get_timeEnteringReady() {
     return this->timeEnteringReady;
 }
 
+int Process::get_level_counter() {
+    return this->level_counter;
+}
+
+int Process::get_total_run() {
+    return this->totalRunTime;
+}
+
 // Setters
 void Process::set_waitTime(int waitTime) {
     this->waitTime = waitTime;
@@ -460,4 +649,26 @@ void Process::set_burst_index(int burst_index) {
 
 void Process::set_timeEnterReady(int timeEnterReady) {
     this->timeEnteringReady = timeEnterReady;
+}
+
+// Helpers
+int Process::calculate_total_run() {
+
+    int totalRun = 0;
+
+    for (int i = 0; i < get_bursts().size(); i++) {
+        totalRun += get_bursts()[i];
+    }
+
+    return totalRun;
+
+}
+void Process::increment_level_counter() {
+    if (level_counter < 2) {
+        level_counter++;
+    }
+}
+
+void Process::reset_level_counter() {
+    level_counter = 0;
 }
